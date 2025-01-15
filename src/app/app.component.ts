27d83c3be618e19021f3e60c0d793701a3f2ecb2
SJH-706 <sjh-706@SJH-706-31.local>
2025-01-15 11:08:10 +0800

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,34 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { interval } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'ng-pipes-demo';
+  price : number = 20000;
+  decimalNum1: number = 8.7589623;
+  decimalNum2: number = 5.43;
+  presentDate = new Date(); 
+  Fruits = ["Apple","Orange","Grapes","Mango","Kiwi","Pomegranate"];
+  time$ = interval(1000).pipe(
+    map(() => new Date())
+  )
+    testObject = {
+      name: 'nath',
+      age: 21,
+      food: 'Cheesecake'
+    };
+    testArray = {
+      name: 'nath',
+      age: 21,
+      food: 'Cheesecake'
+    };
 }
